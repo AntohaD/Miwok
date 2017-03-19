bcd7061bd9dddbe3e6e9695e5e80276b0ed68acd
Anton <anton_dv@list.ru>
2017-03-19 20:26:27 +0200

    Add files via upload

---
+++ src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,43 @@
+package com.example.android.miwok;
+
+public class Word {
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private int mSoundResourceId;
+
+    public Word(String defaultTranslation, String miwokTranslation, int soundResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mSoundResourceId = soundResourceId;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int soundResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mSoundResourceId = soundResourceId;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public int getSoundResourceId() {
+        return mSoundResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+}
